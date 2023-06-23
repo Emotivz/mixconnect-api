b97c4ed262000b18b99242813a9aed785e7f6452
Michael Orimolade <siwsher@hotmail.com>
2023-06-23 17:39:13 +0100

    add events table migration

--- migrations/20230623083239_create_events_table.js
+++ migrations/20230623083239_create_events_table.js
@@ -5,6 +5,17 @@
 exports.up = function (knex) {
   return knex.schema.createTable("events", (table) => {
     table.increments("id").primary();
+    table
+      .integer("user_id")
+      .unsigned()
+      .references("users.id")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+    table.integer("type_id").unsigned().references("type.id");
+    table.string("title").notNullable();
+    table.datetime("date_time").notNullable();
+    table.string("location").notNullable();
+    table.string("cover_photo ").nullable();
   });
 };
 
@@ -12,4 +23,6 @@ exports.up = function (knex) {
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {};
+exports.down = function (knex) {
+  return knex.schema.dropTable("events");
+};
